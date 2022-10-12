c7775ad2fe6d5277e8369c633705913b5d295d42
Harijan Fernandez <har.fernandez@hotmail.com>
2022-10-11 22:18:08 -0300

    primer push con router

---
+++ routes/router.js
@@ -0,0 +1,57 @@
+const express = require('express')
+let router = express.Router()
+module.exports = router
+
+
+
+// trae todos los productos
+
+router
+.get('/',(req,res)=>{
+    contenedor.getAll()
+    .then(productos=>{
+        res.json(productos)
+    })
+})
+
+// trae producto por parametro de id
+.get('/:id',(req,res)=>{
+    const idProducto=parseInt(req.params.id)
+    contenedor.getById(idProducto)
+    .then(prod=>{
+        res.json(prod)
+    })
+})
+
+// agrega producto al array de productos
+.post('/',(req,res)=>{
+    const producto=(req.body)
+    contenedor.save(producto)
+    .then(prod=>{
+        res.json(prod)
+    }) 
+})
+
+// recibe producto y actualiza segun id 
+.put('/:id',(req,res)=>{
+    const idProducto=parseInt(req.params.id)
+    const actualizacion=(req.body) 
+    contenedor.modifyObj(idProducto,actualizacion)
+    .then(prod=>{
+        res.json(prod)
+    })
+})
+
+// borra un elemento segun id 
+.delete('/:id',(req,res)=>{
+    const idProducto=parseInt(req.params.id)
+    contenedor.deleteById(idProducto)
+    .then(prod=>{
+        res.json(prod)
+    })
+})
+.post('/form',(req,res)=>{
+    const actualizacion=(req.body) 
+    res.json(actualizacion)
+})
+
